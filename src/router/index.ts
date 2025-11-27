49a2fe472f7ae7f5d9eb646e588d5fd87dda21c4
terencelee <terence940203@gmail.com>
2025-11-27 16:38:49 +0800

    fix - blog cant open issue

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import type { RouteRecordRaw } from 'vue-router'
 
 import i18n from '@/i18n'
@@ -22,7 +22,7 @@ const routes: RouteRecordRaw[] = [
 ]
 
 const router = createRouter({
-    history: createWebHistory(),
+    history: createWebHashHistory(),
     routes
 })
 
