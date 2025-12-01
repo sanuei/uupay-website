d7fe1013af9f4a0ae4f339687d0847e0875c8eb6
lufei <lufei@gmail.com>
2025-12-01 13:28:47 +0800

    fix: 关键修改

--- src/i18n/locales/en.ts
+++ src/i18n/locales/en.ts
@@ -112,10 +112,10 @@ const en = {
   safetyAnnouncement: 'Security Announcement',
   goBackAnnounce: 'Back to Announcement Center',
   blogTitle: 'Blog',
-  metaTitle: 'UUPAY Bitcoin Card - USDT Virtual Debit Card | Secure Global Visa Payments',
-  metaDescription: 'UUPAY delivers secure bitcoin cards with USDT/USDC deposits for instant Visa/Mastercard use worldwide. Activate in 3 minutes, enjoy 0.5% low fees, and seamless Southeast Asia cross-border spending. Download the app now to apply for your virtual bitcoin card!',
-  metaH1: 'Bitcoin Payment Solutions - UUPAY Bitcoin Card',
-  metaKeywords: 'bitcoin card, bitcoin card, USDT card, virtual bitcoin card, bitcoin visa card, USDT debit card'
+  metaTitle: 'UUPAY Crypto Card - USDT Virtual Debit Card | Secure Global Visa Payments',
+  metaDescription: 'UUPAY delivers secure bitcoin cards with USDT/USDC deposits for instant Visa/Mastercard use worldwide. Activate in 3 minutes, enjoy 0.5% low fees, and seamless Southeast Asia cross-border spending. Download the app now to apply for your virtual crypto card!',
+  metaH1: 'Bitcoin Payment Solutions - UUPAY Crypto Card',
+  metaKeywords: 'crypto card, bitcoin card, USDT card, virtual crypto card, crypto visa card, USDT debit card'
 }
 
 export default en
