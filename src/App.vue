94a5acc3e54c53e3add81355265d94e20074d87d
terencelee <terence940203@gmail.com>
2026-01-09 11:26:06 +0800

    feature - 泰文字体修改

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,21 @@
+<script setup lang="ts">
+import { watch } from 'vue'
+import { useI18n } from 'vue-i18n'
+
+const { locale } = useI18n()
+
+watch(
+    locale,
+    (lang) => {
+      const html = document.documentElement
+
+      html.classList.remove('lang-zh', 'lang-en', 'lang-th')
+      html.classList.add(`lang-${lang}`)
+    },
+    { immediate: true }
+)
+</script>
+
 <template>
   <router-view />
 </template>
